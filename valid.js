d491b5bf2b88eeaf68ef278cb95e1abaf6fdc8a6
raghuveerchintal <raghuveerchintal8@gmail.com>
2020-06-27 23:59:04 +0530

    added home page

--- valid.js
+++ valid.js
@@ -1,52 +1,90 @@
 function validation() {
-    var form = document.getElementById('form');
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var text = document.getElementById('text');
     var text1 = document.getElementById('pass');
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if (email==="") {
-        form.classList.remove('valid');
-        form.classList.remove('invalid');
+    if ((email.length<1) && (password.length<1)){
         text.innerHTML = 'Please fill the Email Address';
-        text.style.color = "#ff0000";
-        
-    }
-
-    else if (email.match(pattern)) {
-        form.classList.add('valid');
-        form.classList.remove('invalid');
-        text.innerHTML = 'Your Email Address in Valid.';
-        text.style.color = "#00ff00";
-        
-    }else{
-        form.classList.remove('valid');
-        form.classList.add('invalid');
-        text.innerHTML="Please Enter Valid Email Address";
-        text.style.color= "#ff0000";
-        
-    }
-    
-    if (password==="") {
-        form.classList.remove('valid');
-        form.classList.remove('invalid');
         text1.innerHTML = 'Please fill the Password';
+        text.style.color = "#ff0000";
         text1.style.color = "#ff0000";
-        
-    }else if (password.length<6){
-        form.classList.remove('valid');
-        form.classList.remove('invalid');
-        text1.innerHTML = 'Password should be atlest 6 characters';
-        text1.style.color = "#ff0000";
-    
-
-    }else{
-        form.classList.add('valid');
-        form.classList.remove('invalid');
-        text1.innerHTML = 'Your Password is valid.';
-        text1.style.color = "#00ff00";
+        return false;
         
     }
+
+    else if ((email.length<1) && (password.length<6)){
+        text.innerHTML = 'Please fill the Email Address';
+        text1.innerHTML = 'Password should be atlest 6 characters';
+        text.style.color = "#ff0000";
+        text1.style.color = "#ff0000";
+        return false;
+    }
+
+    else if ((email.length<1) && (password.length>=6)){
+        text.innerHTML = 'Please fill the Email Address';
+        text1.innerHTML = 'Your Password is Valid';
+        text.style.color = "#ff0000";
+        text1.style.color = "#00ff00";
+        return false;
+    }
+
+
+    else if ((email.match(pattern)) && (password.length<1)) {
+        text.innerHTML = 'Your Email Address is Valid.';
+        text1.innerHTML = 'Please fill the Password';
+        text.style.color = "#00ff00";
+        text1.style.color = "#ff0000";
+        return false;
+        
+    }
+
+
+    else if ((email.match(pattern)) && (password.length>=6)) {
+        text.innerHTML = 'Your Email Address is Valid.';
+        text1.innerHTML = 'Your Password is Valid.';
+        text.style.color = "#00ff00";
+        text1.style.color = "#00ff00";
+        return true;
+        
+    }
+
+    else if ((email.match(pattern)) && (password.length<6)) {
+        text.innerHTML = 'Your Email Address is Valid.';
+        text1.innerHTML = 'Password should be atlest 6 characters';
+        text.style.color = "#00ff00";
+        text1.style.color = "#ff0000";
+        return false;
+        
+    }
+
+    else if (!(email.match(pattern)) && (password.length<1)) {
+        text.innerHTML="Please Enter Valid Email Address";
+        text1.innerHTML = 'Please fill the Password';
+        text.style.color = "#ff0000";
+        text1.style.color = "#ff0000";
+        return false;
+        
+    }
+
+    else if (!(email.match(pattern)) && (password.length>=6)) {
+        text.innerHTML="Please Enter Valid Email Address";
+        text1.innerHTML = 'Your Password is Valid.';
+        text.style.color = "#ff0000";
+        text1.style.color = "#00ff00";
+        return false;
+        
+    }
+    
+
+    else{
+        text.innerHTML="Please Enter Valid Email Address";
+        text1.innerHTML = 'Password should be atlest 6 characters';
+        text.style.color= "#ff0000";
+        text1.style.color = "#ff0000";
+        return false;
+    }
+    
     
 }
