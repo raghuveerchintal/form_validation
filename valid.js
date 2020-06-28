180104baec4ca145029e4b0279a21e0c73f7425e
VedantRD <vedant.debadwar@gmail.com>
2020-06-28 15:58:59 +0530

    fixed validation

--- valid.js
+++ valid.js
@@ -5,86 +5,35 @@ function validation() {
     var text1 = document.getElementById('pass');
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if ((email.length<1) && (password.length<1)){
-        text.innerHTML = 'Please fill the Email Address';
-        text1.innerHTML = 'Please fill the Password';
+    if (email === '' || password === '') {
+        if (email === '') {
+            text.innerHTML = 'Please fill the Email Address';
+            text.style.color = "#ff0000";
+            return false;
+        }
+        if (password === '') {
+            text1.innerHTML = 'Please fill the Password';
+            text1.style.color = "#ff0000";
+            return false;
+        }
+    }
+
+    if (!email.match(pattern)) {
+        text.innerHTML = 'Please enter valid email address';
         text.style.color = "#ff0000";
-        text1.style.color = "#ff0000";
-        return false;
-        
+        return false
     }
 
-    else if ((email.length<1) && (password.length<6)){
-        text.innerHTML = 'Please fill the Email Address';
-        text1.innerHTML = 'Password should be atlest 6 characters';
-        text.style.color = "#ff0000";
-        text1.style.color = "#ff0000";
-        return false;
+    if (password.length < 6) {
+        text1.innerHTML = 'Passworm must be min 6 char'
+        text1.style.color = '#f00'
+        return false
     }
 
-    else if ((email.length<1) && (password.length>=6)){
-        text.innerHTML = 'Please fill the Email Address';
-        text1.innerHTML = 'Your Password is Valid';
-        text.style.color = "#ff0000";
-        text1.style.color = "#00ff00";
-        return false;
-    }
+    return true
 
 
-    else if ((email.match(pattern)) && (password.length<1)) {
-        text.innerHTML = 'Your Email Address is Valid.';
-        text1.innerHTML = 'Please fill the Password';
-        text.style.color = "#00ff00";
-        text1.style.color = "#ff0000";
-        return false;
-        
-    }
 
 
-    else if ((email.match(pattern)) && (password.length>=6)) {
-        text.innerHTML = 'Your Email Address is Valid.';
-        text1.innerHTML = 'Your Password is Valid.';
-        text.style.color = "#00ff00";
-        text1.style.color = "#00ff00";
-        return true;
-        
-    }
 
-    else if ((email.match(pattern)) && (password.length<6)) {
-        text.innerHTML = 'Your Email Address is Valid.';
-        text1.innerHTML = 'Password should be atlest 6 characters';
-        text.style.color = "#00ff00";
-        text1.style.color = "#ff0000";
-        return false;
-        
-    }
-
-    else if (!(email.match(pattern)) && (password.length<1)) {
-        text.innerHTML="Please Enter Valid Email Address";
-        text1.innerHTML = 'Please fill the Password';
-        text.style.color = "#ff0000";
-        text1.style.color = "#ff0000";
-        return false;
-        
-    }
-
-    else if (!(email.match(pattern)) && (password.length>=6)) {
-        text.innerHTML="Please Enter Valid Email Address";
-        text1.innerHTML = 'Your Password is Valid.';
-        text.style.color = "#ff0000";
-        text1.style.color = "#00ff00";
-        return false;
-        
-    }
-    
-
-    else{
-        text.innerHTML="Please Enter Valid Email Address";
-        text1.innerHTML = 'Password should be atlest 6 characters';
-        text.style.color= "#ff0000";
-        text1.style.color = "#ff0000";
-        return false;
-    }
-    
-    
 }
